6b59fe1a19db93e98e8aeeeca542a88120d8d0bb
Evgeny Bychkov <jdexmoment@vk.com>
2023-11-25 21:59:22 +0300

    добавил анимацию
    а также применил открытие формы и её анимации ко всем кнопкам

--- script/script.js
+++ script/script.js
@@ -24,3 +24,43 @@ const slide = () => {
 window.addEventListener('load', () => {
     slide();
 });
+
+function showMenu() {
+    // Показываем высвечивающееся меню
+    var menuOverlay = document.getElementById('menuOverlay');
+    var menuContent = document.getElementById('menuContent');
+    menuOverlay.style.display = 'flex';
+    setTimeout(() => {
+      menuOverlay.style.opacity = 1;
+      menuContent.style.transform = 'scale(1)';
+      menuContent.style.opacity = 1;
+    }, 10);
+  }
+
+  function hideMenu() {
+    // Скрываем высвечивающееся меню
+    var menuOverlay = document.getElementById('menuOverlay');
+    var menuContent = document.getElementById('menuContent');
+
+    menuOverlay.style.opacity = 0;
+    menuContent.style.transform = 'scale(0.8)';
+    menuContent.style.opacity = 0;
+
+    // Добавляем обработчик событий transitionend для скрытия после завершения анимации
+    menuOverlay.addEventListener('transitionend', function() {
+      menuOverlay.style.display = 'none';
+    }, { once: true });
+  }
+
+  function submitForm() {
+    // Здесь вы можете добавить код для отправки данных на сервер
+    var name = document.getElementById('name').value;
+    var phone = document.getElementById('phone').value;
+    var service = document.getElementById('service').value;
+
+    // Ваш код для отправки данных, например, через AJAX
+    console.log('Отправка данных:', name, phone, service);
+
+    // Закрываем меню после отправки
+    hideMenu();
+  }
